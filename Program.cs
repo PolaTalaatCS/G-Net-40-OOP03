06bbcde1403b987ecf98bc07d88ecdc913de30af
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-25 19:02:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace G_Net_40_OOP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1 
+            // a) compsotion
+            // b) association
+            // c) inheritance
+            // d) aggregation
+            // e) dependency
+            #endregion
+        }
+    }
+}
