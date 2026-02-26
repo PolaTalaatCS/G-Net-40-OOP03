b7c231b94c5515c60609a4e7e7d22fa5361d992e
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-26 17:11:01 +0200

    part 2 question 3

--- Ticket.cs
+++ Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,8 @@ namespace G_Net_40_OOP03
         public string Movieame { get; set; }
         public decimal Price
         {
-            get { return price; } set
+            get { return price; }
+            set
             {
                 if (value > 0)
                 {
@@ -24,7 +26,8 @@ namespace G_Net_40_OOP03
                 }
             }
         }
-        public int Ticketid { 
+        public int Ticketid
+        {
             get
             {
                 idcounter++;
@@ -52,7 +55,7 @@ namespace G_Net_40_OOP03
         #region part 2 question 2
         public class standardticket : Ticket
         {
-           private string seatnumber;
+            private string seatnumber;
             public string Seatnumber { get; set; }
             public standardticket(string _moviename, decimal _price, string _seatnumber) : base(_moviename, _price)
             {
@@ -65,7 +68,7 @@ namespace G_Net_40_OOP03
 
             public class Vipticket : Ticket
             {
-                private  bool laungeaccess;
+                private bool laungeaccess;
                 static decimal servicefee = 50;
                 public bool Laungeaccess { get; set; }
                 public Vipticket(string _moviename, decimal _price, bool _laungeaccess) : base(_moviename, _price)
@@ -106,7 +109,66 @@ namespace G_Net_40_OOP03
             }
             #endregion
         }
+        #region Part 2 question 3
+        public class Projector
+        {
+            public void Start()
+            {
+                Console.WriteLine("Projector started.");
+            }
 
+            public void Stop()
+            {
+                Console.WriteLine("Projector stopped.");
+            }
+        }
+        public class Cinema
+        {
+            private Ticket[] tickets = new Ticket[20];
+
+            public string CinemaName { get; set; }
+
+            public Projector Projector { get; }
+
+            public Cinema(string name)
+            {
+                CinemaName = name;
+                Projector = new Projector();
+            }
+            public bool AddTicket(Ticket t)
+            {
+                for (int i = 0; i < tickets.Length; i++)
+                {
+                    if (tickets[i] == null)
+                    {
+                        tickets[i] = t;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public void PrintAllTickets()
+            {
+                foreach (var ticket in tickets)
+                {
+                    if (ticket != null)
+                    {
+                        Console.WriteLine(ticket);
+                    }
+                }
+            }
+            public void OpenCinema()
+            {
+                Console.WriteLine($"Cinema {CinemaName} is now open.");
+                Projector.Start();
+            }
+            public void CloseCinema()
+            {
+                Console.WriteLine($"Cinema {CinemaName} is now closed.");
+                Projector.Stop();
+            }
+        }
+        #endregion
     }
 
 }
