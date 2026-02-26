a21b16fa984877124bf99c6e45a283126e57af30
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-26 16:45:57 +0200

    part 2 question 2

--- Ticket.cs
+++ Ticket.cs
@@ -49,6 +49,63 @@ namespace G_Net_40_OOP03
             return idcounter;
         }
         #endregion
+        #region part 2 question 2
+        public class standardticket : Ticket
+        {
+           private string seatnumber;
+            public string Seatnumber { get; set; }
+            public standardticket(string _moviename, decimal _price, string _seatnumber) : base(_moviename, _price)
+            {
+                seatnumber = _seatnumber;
+            }
+            public override string ToString()
+            {
+                return base.ToString() + $", Seat number: {seatnumber}";
+            }
+
+            public class Vipticket : Ticket
+            {
+                private  bool laungeaccess;
+                static decimal servicefee = 50;
+                public bool Laungeaccess { get; set; }
+                public Vipticket(string _moviename, decimal _price, bool _laungeaccess) : base(_moviename, _price)
+                {
+                    laungeaccess = _laungeaccess;
+                }
+                public override string ToString()
+                {
+                    return base.ToString() + $", Launge access: {laungeaccess}, Service fee: {servicefee}";
+                }
+            }
+            public class imaxticket : Ticket
+            {
+                private string screen;
+                static decimal imaxfee = 30;
+                public string Screen { get; set; }
+                public imaxticket(string _moviename, decimal _price, string _screen) : base(_moviename, _price)
+                {
+                    screen = _screen;
+                }
+                public override string ToString()
+                {
+                    return base.ToString() + $", Screen: {screen}, IMAX fee: {imaxfee}";
+                }
+            }
+            public class Iamxticket : Ticket
+            {
+                private bool is3d;
+                public bool iS3D { get; set; }
+                public Iamxticket(string _moviename, decimal _price, bool _is3d) : base(_moviename, _price)
+                {
+                    is3d = _is3d;
+                }
+                public override string ToString()
+                {
+                    return base.ToString() + $" Is 3D: {is3d}";
+                }
+            }
+            #endregion
+        }
 
     }
 
