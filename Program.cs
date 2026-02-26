9f25c96d6115700c4409995dfc4b5df5c10f7428
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-26 17:38:43 +0200

    question 2

--- Program.cs
+++ Program.cs
@@ -15,13 +15,13 @@ namespace G_Net_40_OOP03
             // e) dependency
             #endregion
             #region Question 2
-            // a) yes Can a child class in a different assembly access it , No, you can’t access it using an object from outside.
+            // a)) yes Can a child class in a different assembly access it , No, you can’t access it using an object from outside.
 
-            // b)protected internal is same assembly or derived class in another assembly,private protected is only derived classes inside the same assembly
+            // b))protected internal is same assembly or derived class in another assembly,private protected is only derived classes inside the same assembly
 
-            //c)sealed class is cannot be inherited,sealed method is cannot be overridden again
+            //c))sealed class is cannot be inherited,sealed method is cannot be overridden again
 
-            //d)yes you can create an object from a sealed class using new sealed only prevents inheritance not creation
+            //d)))yes you can create an object from a sealed class using new sealed only prevents inheritance not creation
             #endregion
             #region part 2 question 4
             Console.WriteLine("==============================cinema opened========================");
