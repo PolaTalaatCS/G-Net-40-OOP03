7bf80cf8ed9e0dd17a8f5777ba620047842dfcb2
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-02-26 16:29:05 +0200

    part 2 question 1

--- Program.cs
+++ Program.cs
@@ -11,6 +11,15 @@
             // d) aggregation
             // e) dependency
             #endregion
+            #region Question 2
+            // a) yes Can a child class in a different assembly access it , No, you can’t access it using an object from outside.
+
+           // b)protected internal is same assembly or derived class in another assembly,private protected is only derived classes inside the same assembly
+
+//c)sealed class is cannot be inherited,sealed method is cannot be overridden again
+
+//d)yes you can create an object from a sealed class using new sealed only prevents inheritance not creation
+            #endregion
         }
     }
 }
---
+++ Ticket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_OOP03
+{
+    public class Ticket
+    {
+        #region part 2 question 1
+        private string moviename;
+        private decimal price;
+        private int ticketid;
+        static int idcounter = 0;
+        public string Movieame { get; set; }
+        public decimal Price
+        {
+            get { return price; } set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
+        public int Ticketid { 
+            get
+            {
+                idcounter++;
+                return idcounter;
+            }
+        }
+        public Ticket(string _moviename, decimal _price)
+        {
+            moviename = _moviename;
+            price = _price;
+        }
+        public decimal Priceaftertax()
+        {
+            return price + (price * 0.14m);
+        }
+        public override string ToString()
+        {
+            return $"Movie name: {moviename}, Price: {price}, Ticket id: {Ticketid}";
+        }
+        static int GetTotalTickets()
+        {
+            return idcounter;
+        }
+        #endregion
+
+    }
+
+}
